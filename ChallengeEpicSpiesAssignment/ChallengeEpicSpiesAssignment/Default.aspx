3c1879bc8a73023215952dda7d3be40dec59583e
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-14 13:53:01 +0100

    Added lastest challenge
    Added challenge and overcome the 0 calandar day issue.

---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+        </div>
+        <p>
+            &nbsp;</p>
+        <h1>Spy New Assignment Form</h1>
+        <p>
+            &nbsp;</p>
+        <p>
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="spyTextBox" runat="server" OnTextChanged="spyTextBox_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="assignmentTextBox" runat="server" OnTextChanged="assignmentTextBox_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            End Date Of Previous Assignment:</p>
+        <p>
+            <asp:Calendar ID="previousAssignment" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Start Date of New Assignment:</p>
+        <p>
+            <asp:Calendar ID="newAssignment" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Projected End Date Of New Assignment:</p>
+        <p>
+            <asp:Calendar ID="newAssignmentEnd" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="clauseLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                previousAssignment.SelectedDate = DateTime.Now.Date;
+                newAssignment.SelectedDate = DateTime.Now.Date.AddDays(14);
+                newAssignmentEnd.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+
+        }
+
+        protected void spyTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignmentTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+            //We first work out the charging rate
+            TimeSpan totalDurationOfAssignment = newAssignmentEnd.SelectedDate.Subtract(newAssignment.SelectedDate);//take from expected end date to start date and subtract to work out the length of the contract
+            //We take a new variable called totalCost and multiply total days of assignment by our rate per day.
+
+            double totalCost = totalDurationOfAssignment.TotalDays * 500.00;
+
+            //This conditional statement added as if we complete the assignment the same day the application would not assign payment without this expression as even though it is technically one day the calendar will say 0 days.
+            if(totalDurationOfAssignment.TotalDays == 0)
+            {
+                totalCost = 500;
+            }
+
+            //If our task exceeds 21 days we pay a £1000 bonus
+            if(totalDurationOfAssignment.TotalDays > 21)
+            {
+                //we use the += to add £1000 to the totalCost value.
+                totalCost += 1000.00;
+            }
+            //We assign the totals and written assignment to our output seperated by , to indicate 0-1-2 (array assignment of options)
+            resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorised. Budget total: {2:C}",
+            spyTextBox.Text,
+            assignmentTextBox.Text,
+            totalCost);
+            //We use this to workout the time between assignments so subtract the difference in dates from the old to the new and out put error if not within range.
+            TimeSpan timeBetweenAssignments = newAssignment.SelectedDate.Subtract(previousAssignment.SelectedDate);
+            if(timeBetweenAssignments.TotalDays < 14)
+            {
+                //error is displayed if timebetween assignments is less than two weeks we were assigned by the union.
+                resultLabel.Text = "Error: Must allow at least two weeks between " + "previous assignment and new assignment.";
+
+                //We set this to state it needs to be 14 days since the last assignment
+                DateTime earliestNewAssignmentDate = previousAssignment.SelectedDate.AddDays(14);
+
+                newAssignment.SelectedDate = earliestNewAssignmentDate;
+                newAssignment.VisibleDate = earliestNewAssignmentDate;
+                    
+            }//Added to ensure a deposit of 30% is requested before accepting a job with a value over £10K
+            if(totalCost >= 10000 )
+            {
+                double deposit;
+
+                deposit = (totalCost / 100) * 30;
+
+                clauseLabel.Text = String.Format("Please collect a deposit of {0:C}",
+                    deposit);
+            }
+            if(totalCost <10000)
+            {
+                clauseLabel.Text = String.Format("");
+            }
+
+
+        }
+    }
+}
