b31467ab9d4cf419876bb8f62bd0701563de7491
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-14 12:25:20 +0100

    Updated

--- CS-ASP_019/CS-ASP_019/Default.aspx.cs
+++ CS-ASP_019/CS-ASP_019/Default.aspx.cs
@@ -19,12 +19,14 @@ namespace CS_ASP_019
             int ss = int.Parse(ssTextBox.Text);
             int phone = int.Parse(phoneTextBox.Text);
 
-            string result = string.Format("Thank you, {0}, for your business. <br />Your Social Security number is: {1:000-00-0000}" +
-                "<br />Phone: {2: (000) 000-0000}", 
+            string result = string.Format("Thank you, {0}, for your business." +
+                "<br />Your Social Security number is: {1:000-00-0000}" +
+                "<br />Phone: {2: (000) 000-0000}" +
+                "<br />Loan Date: {3:ddd -- d, M, yy}", 
                 nameTextBox.Text, 
-                ssTextBox.Text, 
                 ss,
-                phone);
+                phone,
+                loanDateCalendar.SelectedDate);
             resultLabel.Text = result;
         }
     }
