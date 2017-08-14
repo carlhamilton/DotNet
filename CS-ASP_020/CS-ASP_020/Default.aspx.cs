3b39c457ad842b72fad251ebeab15cf80e90a515
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-14 16:08:59 +0100

    Added ViewState
    Retaining webform entries.

---
+++ CS-ASP_020/CS-ASP_020/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_020
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                //we call a viewstate and add the value that we choose in the addButton function
+                ViewState.Add("MyValue", "");
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            //we add the valueTextBox details into the resultLabel to sow the enter
+            string value = ViewState["MyValue"].ToString();
+            value += valueTextBox.Text + " ";
+            ViewState["MyValue"] = value;
+            resultLabel.Text = value;
+
+            valueTextBox.Text = "";
+        }
+    }
+}
