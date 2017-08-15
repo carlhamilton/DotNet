0122f485befb8eb8067b41d9c4171b07f72cbe6a
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-15 09:16:21 +0100

    Added New Challenge
    Added New Challenge

---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="178px" ImageUrl="~/epic-spies-logo.jpg" Width="150px" />
+            <br />
+            <br />
+            <h1>Asset Performance Tracker</h1>
+        </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetTextBox" runat="server" OnTextChanged="assetTextBox_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Elections Rigged:&nbsp;
+            <asp:TextBox ID="electionTextBox" runat="server" OnTextChanged="electionTextBox_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Acts Of Subterfuge Performed:&nbsp;
+            <asp:TextBox ID="aospTextBox" runat="server" OnTextChanged="aospTextBox_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="assetButton" runat="server" OnClick="assetButton_Click" Text="Add Asset" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)//Condition to set to prevent contstant re-initialising of varibles.
+            {
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] aosp = new int[0];
+
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("AOSP", aosp);
+
+
+            }
+
+        }
+
+        protected void assetTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void electionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void aospTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assetButton_Click(object sender, EventArgs e)
+        {
+            string[] assets = (string[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] aosp = (int[])ViewState["AOSP"];
+
+            int newLength = assets.Length + 1;
+
+            //We re-size the arrays with an array reference and specify the new lenght of the array by adding 1 to the previous array length
+            Array.Resize(ref assets, newLength);
+            Array.Resize(ref elections, newLength);
+            Array.Resize(ref aosp, newLength);
+
+            int newIndex = assets.GetUpperBound(0);
+
+            assets[newIndex] = assetTextBox.Text;
+            elections[newIndex] = int.Parse(electionTextBox.Text);
+            aosp[newIndex] = int.Parse(aospTextBox.Text);
+
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["AOSP"] = aosp;
+            //N2 is used to display 2 decimal places
+            resultLabel.Text = String.Format("Total Elections Rigged: {0}<br />Average Acts of Subterfuge per Asset: {1:N2}<br />(Last Asset you Added: {2})",
+                elections.Sum(),//We use this to add all the integers stored in the election array
+                aosp.Average(),//discover the average
+                assets[newIndex]);
+
+            //We then clear the text boxes so the next entry can be added.
+            assetTextBox.Text = "";
+            electionTextBox.Text = "";
+            aospTextBox.Text = "";
+
+        }
+    }
+}
