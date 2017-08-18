c757eec529e52e7fde7ad0f9bbb73df5f74e666f
Carl Matthew Hamilton <info@chtsi.uk>
2017-08-18 22:49:05 +0100

    update

---
+++ CS-ASP-024/CS-ASP-024/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_024
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            // x is accessible to to this method and the if statement
+            int x = 1;
+
+            if(x ==1)
+            {
+                x = 2;
+                int y = 3;
+            }
+
+            //we can use x as it was defined but if we used y it would not be available.
+            string result = x.ToString();
+        
+        }
+    }
+}
---
+++ CS-ASP_026/CS-ASP_026/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_026
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                result += i.ToString() + " | ";
+            }
+            resultLabel.Text = result;
+        }
+    }
+}
